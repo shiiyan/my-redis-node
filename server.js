bd40af951e323fef83d5c64690b4c52d565f5997
yan <yan39123@gmail.com>
2022-03-05 20:57:23 +0900

    init server.js

--- server.js
+++ server.js
@@ -6,3 +6,47 @@ const axios = require('axios')
 
 const app = express()
 const redisClient = redis.createClient('redis://localhost:6379')
+
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
+  res.send(`
+  <html lang="en">
+    <head>
+      <meta charset="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+      <title>My Homepage</title>
+    </head>
+    <body>
+      Hi there
+      <form action="/" method="post">
+        <input type="number" name="number" placeholder="a number" />
+        <input type="submit" />
+      </form>
+    </body>
+  </html>
+  `)
+})
+
+// app.post('/', (req, res) => {
+//   let number = req.body.number
+
+//   redisClient.exists(number, (error, result) => {
+//     if (result) {
+
+//     } else {
+
+//     }
+//   })
+
+// })
+
+// app.get('/done', (req, res) => {
+//   res.send(`
+//   `)
+// })
+
+
+app.listen(8080)
